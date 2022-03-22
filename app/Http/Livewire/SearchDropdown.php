a72e0116f895207d676d39fce330c4c884471e4d
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2022-03-22 23:17:51 +0000

    remove extra code and comment

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -14,26 +14,14 @@ class SearchDropdown extends Component
     {
         $searchResults = [];
 
-        if (strlen($this->search) >= 2) {
-            $movies = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/search/movie?query=' . $this->search)
-            ->json()['results'];
-
-            $tvshows = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/search/tv?query=' . $this->search)
-            ->json()['results'];
-
+        if (strlen($this->search) >= 2) {;
             $searchResults = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/search/multi?query=' . $this->search)
             ->json()['results'];
-            // dd($searchResults);
         }
 
         $viewModel = new SearchViewModel($searchResults);
        
         return view('livewire.search-dropdown', $viewModel);
-        // return view('livewire.search-dropdown', [
-        //     'searchResults' => collect($searchResults)->take(7)
-        // ]);
     }
 }
